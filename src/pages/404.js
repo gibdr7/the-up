62b93b40c9ef4b5bb1252a3a3bcd5f8a23243229
Drew Gibson <gibdr7@gmail.com>
2020-03-05 17:39:46 -0500

    add style to 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import Layout from '../components/layout'
+import '../components/main-style.scss'
 
 const NotFoundPage = () => (
   <Layout>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <div className="not-found-box">
+      <h1 className="has-text-centered is-title is-size-1 is-uppercase">Page not found</h1>
+      <p className="has-text-centered is-size-3">You just hit a route that doesn&#39;t exist... the sadness.</p>
+    </div>
   </Layout>
 )
 
