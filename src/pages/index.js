2e274aef2902d4c896d6cc40878ebca866107076
Drew Gibson <gibdr7@gmail.com>
2020-03-29 14:10:45 -0400

    remove 'midsection'

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,12 @@
 import React from 'react'
 
 import '../components/main-style.scss'
-import Midsection from '../components/Midsection/midsection'
 import Layout from '../components/layout'
 import Header from '../components/Header/header'
 
 const IndexPage = () => (
   <Layout>
     <Header />
-    <Midsection />
   </Layout>
 )
 
