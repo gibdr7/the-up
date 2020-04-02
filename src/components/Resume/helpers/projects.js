89ebedf486176ef78c5f9f48f91d54f3b09146f1
Drew Gibson <gibdr7@gmail.com>
2020-04-02 17:59:51 -0400

    WIP-nothing showing

---
+++ src/components/Resume/helpers/projects.js
@@ -0,0 +1,37 @@
+import React from 'react'
+// import '../resume.scss'
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from 'react-vertical-timeline-component'
+import 'react-vertical-timeline-component/style.min.css'
+import { FaSuitcase, FaSchool, FaStar } from 'react-icons/fa'
+
+const Projects = () => {
+  return (
+    // <div>test</div>
+    <VerticalTimeline>
+      <VerticalTimelineElement
+        className="vertical-timeline-element--work"
+        contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+        contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
+        date="2011 - present"
+        iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+        icon={<FaSuitcase />}
+      >
+        <h3 className="vertical-timeline-element-title">Creative Director</h3>
+        <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
+        <p>
+          Creative Direction, User Experience, Visual Design, Project
+          Management, Team Leading
+        </p>
+      </VerticalTimelineElement>
+      <VerticalTimelineElement
+        iconStyle={{ background: 'rgb(16, 204, 82)', color: '#fff' }}
+        icon={<FaStar />}
+      />
+    </VerticalTimeline>
+  )
+}
+
+export default Projects
--- src/pages/resume.js
+++ src/pages/resume.js
@@ -1,132 +1,13 @@
 import React from 'react'
 
 import Layout from '../components/layout'
+import Resume from '../components/Resume/resume'
 
-import './resume.scss'
 
-import { StaticQuery, graphql } from 'gatsby'
-import SocialContainer from '../components/SocialContainer/socialContainer'
-
-const skills = [
-  {
-    name: 'React',
-    level: 40,
-  },
-  {
-    name: 'CSS',
-    level: 40,
-  },
-  {
-    name: 'Javascript',
-    level: 45,
-  },
-  {
-    name: 'Python',
-    level: 60,
-  },
-  {
-    name: 'SQL',
-    level: 60,
-  },
-  {
-    name: 'Git',
-    level: 40,
-  },
-]
-
-const jobs = [
-  {
-    company: 'Lab49',
-    begin: {
-      month: 'oct',
-      year: '2019',
-    },
-    duration: null,
-    occupation: 'Development Consultant',
-    description:
-      'I do stuff.',
-  },
-  {
-    company: 'ION Group',
-    begin: {
-      month: 'jul',
-      year: '2018',
-    },
-    duration: null,
-    occupation: 'Corporate Development | M&A Analyst',
-    description:
-      'I do stuff.',
-  },
-  {
-    company: 'Adobe',
-    begin: {
-      month: 'may',
-      year: '2016',
-    },
-    duration: '6 mos',
-    occupation: 'Innovation Analyst',
-    description:
-      'I do stuff.',
-  },
-]
-
-const Resume = () => (
-  <StaticQuery
-    query={graphql`
-      query resumeQuery {
-        site {
-          siteMetadata {
-            author
-            title
-          }
-        }
-      }
-    `}
-    render={data => (
-      <Layout title="Resume">
-        <h1>Picture of me</h1>
-        <h1>Bio description (linkedin)</h1>
-        <div className="social">
-          <SocialContainer/>
-        </div>
-        <div>
-          <h1>{data.site.siteMetadata.title}</h1>
-          {skills.map(skill => (
-            <div>
-              <label>{skill.name}</label>
-              <div id={`${skill.name}-bar`} className="skill__bar">
-                <div className="skill__level"></div>
-              </div>
-            </div>
-          ))}
-        </div>
-        <div>
-          <h1>Experience</h1>
-          {jobs &&
-            jobs.map(job => (
-              <article
-                key={job.begin.month + job.begin.year}
-                className="timeline__item"
-              >
-                <div className="inner">
-                  <span className="timeline__date">
-                    <span className="timeline__month">{job.begin.month}</span>
-                    <span className="timeline__year">{job.begin.year}</span>
-                  </span>
-                  <h2 className="timeline__title">
-                    {job.occupation} at {job.company} <br />
-                    <small className="timeline__title--small">
-                      ({job.duration || 'present'})
-                    </small>
-                  </h2>
-                  <p>{job.description}</p>
-                </div>
-              </article>
-            ))}
-        </div>
-      </Layout>
-    )}
-  />
+const ResumePage = () => (
+  <Layout title="Resume">
+    <Resume />
+  </Layout>
 )
 
-export default Resume
+export default ResumePage;
